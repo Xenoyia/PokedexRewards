bb12b663a8d91df6570e331e9463eb8da5429b83
Xenoyia <ccplukeg@gmail.com>
2017-05-04 01:15:45 +0100

    First Commit

---
+++ src/com/xpgaming/xPPokeDex/Main.java
@@ -0,0 +1,11 @@
+package com.xpgaming.xPPokeDex;
+
+@Plugin(id = "xppokedex", name = "xP// PokéDex", version = "0.1")
+public class Main {
+    @Listener
+    public void onServerStart(GameStartedServerEvent event) {
+        // Hey! The server has started!
+        // Try instantiating your logger in here.
+        // (There's a guide for that)
+    }
+}
