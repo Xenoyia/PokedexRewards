9b197fef731cd6cf942742a89d73ae3c0b78df89
Xenoyia <ccplukeg@gmail.com>
2018-02-14 00:08:06 +0000

    nomenclature again, also shinies now automatically update in your list

--- src/main/java/com/xpgaming/xPPokeDex/PokedexBase.java
+++ src/main/java/com/xpgaming/xPPokeDex/PokedexBase.java
@@ -14,15 +14,15 @@ public class PokedexBase implements CommandExecutor {
             Player player = (Player) src;
             player.sendMessage(Text.of("\u00A7f[\u00A7bPokeDex\u00A7f] \u00A7b\u00A7l-- COMMANDS --"));
             if (player.hasPermission("xpgaming.pokedex.base")) {
-                player.sendMessage(Text.of("  \u00A77> \u00A7b/pokedex count\u00A7f|\u00A7bc \u00A77- Count remaining Pokemon to catch!"));
-                player.sendMessage(Text.of("  \u00A77> \u00A7b/pokedex remaining\u00A7f|\u00A7br \u00A77- List remaining Pokemon to catch!"));
+                player.sendMessage(Text.of("  \u00A77> \u00A7b/pokedex count\u00A7f|\u00A7bc \u00A77- Count remaining Pokémon to catch!"));
+                player.sendMessage(Text.of("  \u00A77> \u00A7b/pokedex remaining\u00A7f|\u00A7br \u00A77- List remaining Pokémon to catch!"));
                 player.sendMessage(Text.of("  \u00A77> \u00A7b/pokedex claim\u00A7f|\u00A7bcl \u00A77- Claim rewards every 10%!"));
                 player.sendMessage(Text.of("  \u00A77> \u00A7b/pokedex convert <slot> \u00A77- Use a shiny token!"));
             }
         } else {
             src.sendMessage(Text.of("\u00A7f[\u00A7bPokeDex\u00A7f] \u00A7b\u00A7l-- COMMANDS --"));
-            src.sendMessage(Text.of("  \u00A77> \u00A7b/pokedex count\u00A7f|\u00A7bc \u00A77- Count remaining Pokemon to catch!"));
-            src.sendMessage(Text.of("  \u00A77> \u00A7b/pokedex remaining\u00A7f|\u00A7br \u00A77- List remaining Pokemon to catch!"));
+            src.sendMessage(Text.of("  \u00A77> \u00A7b/pokedex count\u00A7f|\u00A7bc \u00A77- Count remaining Pokémon to catch!"));
+            src.sendMessage(Text.of("  \u00A77> \u00A7b/pokedex remaining\u00A7f|\u00A7br \u00A77- List remaining Pokémon to catch!"));
             src.sendMessage(Text.of("  \u00A77> \u00A7b/pokedex claim\u00A7f|\u00A7bcl \u00A77- Claim rewards every 10%!"));
             src.sendMessage(Text.of("  \u00A77> \u00A7b/pokedex convert <slot> \u00A77- Use a shiny token!"));
         }
